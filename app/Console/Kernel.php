26a5f87db84834fb89938c089887539171ee86bc
VANDEW04Lo <VANDEW04Lo@lycee-ndduroc.com>
2022-04-05 12:16:17 +0200

    schedule création auto expensesheet et cloturation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\Employee;
+use App\Models\Expensepackagetype;
+use App\Models\Expensesheet;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +19,35 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function (){
+            $Employees = Employee::all();
+            foreach ($Employees as $employee){
+                $expenseSheet['employee_id']=$employee->id;
+                $expenseSheet['sheetstate_id']=1;
+                $expenseSheet['creationDate']=Carbon::now()->addMonth()->format('Y-m-d');
+                $expenseSheet['unit']='€';
+                $date =Carbon::now()->format('Y-m-d');
+                $date[6] =$date[6]+1;
+                if($date[5]==1 && $date[6]!=3){
+                    $ref = $date[2].$date[3].$date[5].$date[6];
+                }
+                elseif($date[5]==1 && $date[6]==3){
+                    $ref = $date[2].$date[3].'0'.($date[6]-1);
+                }
+                else{
+                    $ref = $date[2].$date[3].'0'.$date[6];
+                }
+                $expenseSheet['ref']=$ref;
+                Expensesheet::create($expenseSheet);
+            }
+        })->monthlyOn(28, '00:00')->timezone('Europe/Paris')->evenInMaintenanceMode();
+        $schedule->call(function(){
+            $expenseSheets = Expensesheet::all();
+            foreach ($expenseSheets as $expenseSheet){
+                $date =Carbon::now()->subMonth()->format('Y-m'.'-28');
+                Expensesheet::where('creationDate',$date)->update(['sheetstate_id'=>2]);
+            }
+        })->monthlyOn(10, '00:00')->timezone('Europe/Paris')->evenInMaintenanceMode();
     }
 
     /**
